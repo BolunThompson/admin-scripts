08c1c341ad22c2b74af9b1a9df42a1a2dda29ac6
Bolun Thompson <Bolun Thompson>
2024-08-18 10:57:01 -0700

    add dot file commit and update function

--- public_lib.sh
+++ public_lib.sh
@@ -9,3 +9,16 @@ private_ip() {
 public_ip() {
   curl -s https://ipinfo.io/ip -w "\n"
 }
+
+# factored out in case I switch password managers
+ssh_pass() {
+  bw get password SSH
+}
+
+update_dot() {
+  cd /etc/nixos/ || return
+  git add home.nix configuration.nix flake.nix
+  git commit -m "$1"
+  git push <<< "$(ssh_pass)"
+  sudo nixos-rebuild switch
+}
