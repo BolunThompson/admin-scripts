f0df7b47eb4e4ce0de5c7f0d8be5991731097890
Bolun Thompson <Bolun Thompson>
2024-08-18 17:18:29 -0700

    refactor flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
   outputs = { self, nixpkgs }: {
 
-    defaultPackage.x86_64-linux =
+    packages.x86_64-linux.default =
 
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation rec {
