ce2d973ab1c40d3204e9459961c7124c9571db6c
Bolun Thompson <me@bolun.dev>
2024-08-21 12:24:23 -0700

    Add proper devshell to flake.

--- flake.nix
+++ flake.nix
@@ -4,41 +4,72 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
 
-    packages.x86_64-linux.default =
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
 
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "admin-scripts";
-        version = "1.0";
-        src = ./.;
+    in
+    {
+      packages = forAllSystems
+        ({ pkgs }: {
 
-        nativeBuildInputs = [ makeWrapper ];
-        buildInputs = [ coreutils ];
+          default = with pkgs; stdenv.mkDerivation {
+            name = "admin-scripts";
+            version = "1.0";
+            src = ./.;
 
-        postInstall =
-          let
-            wrapperPath = lib.makeBinPath [
-              coreutils
-              git
-              bitwarden-cli
-              sshpass
-              openssh
-            ];
-          in
-          ''
-            for program in $out/bin/*; do
-              wrapProgram "$program" --prefix PATH : "${wrapperPath}"
-            done
-          '';
-        installPhase = ''
-          runHook preInstall
-          mkdir -p "$out"
-          cp -r ./bin "$out"
-          cp lib.sh "$out"
-          runHook postInstall
-        '';
-      };
-  };
+            nativeBuildInputs = [ coreutils makeWrapper ];
+
+            # TODO: Create dev shell
+
+            postInstall =
+              let
+                wrapperPath = lib.makeBinPath [
+                  coreutils
+                  bash
+                  git
+                  bitwarden-cli
+                  sshpass
+                  openssh
+                ];
+              in
+              ''
+                for program in $out/bin/*; do
+                  wrapProgram "$program" --prefix PATH : "${wrapperPath}"
+                done
+              '';
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out"
+              cp -r ./bin "$out"
+              cp lib.sh "$out"
+              runHook postInstall
+            '';
+          };
+        }
+        );
+
+      devShells = forAllSystems
+        (
+          { pkgs }: {
+            default = with pkgs; mkShell {
+              packages = [
+                shfmt
+                shellcheck
+                nodePackages_latest.bash-language-server
+              ];
+            };
+          }
+        );
+    };
 }
