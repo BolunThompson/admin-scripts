da48ff64a1299a5150848c6feabdba5dca2be512
Bolun Thompson <me@bolun.dev>
2025-05-05 15:34:01 -0700

    Add code_to_pdf script

--- flake.nix
+++ flake.nix
@@ -1,78 +1,65 @@
 {
-
   description = "admin-scripts";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs = {
+    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python312.withPackages (ppkgs: with ppkgs; [
+          ansi2html
+          weasyprint
+        ]);
+
+        # Runtime dependencies that will be added to every wrapped script’s PATH
+        runtimePkgs = with pkgs; [
+          coreutils
+          bash
+          git
+          bitwarden-cli
+          sshpass
+          openssh
+          findutils
+          gnugrep
+          gnused
+          wkhtmltopdf
+          bat
+          python
+        ];
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname   = "admin-scripts";
+          version = "1.0";
+          src     = ./.;
+
+          nativeBuildInputs = with pkgs; [ coreutils makeWrapper gnused ];
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p "$out"
+            cp -r ./bin "$out"
+            cp lib.sh "$out"
+            runHook postInstall
+          '';
+
+          postInstall = let wrapperPath = pkgs.lib.makeBinPath runtimePkgs; in ''
+            for program in "$out"/bin/*; do
+              wrapProgram "$program" --prefix PATH : "${wrapperPath}"
+            done
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            shfmt
+            shellcheck
+            nodePackages_latest.bash-language-server
+          ] ++ runtimePkgs;
+        };
       });
-
-    in
-    {
-      packages = forAllSystems
-        ({ pkgs }: {
-
-          default = with pkgs; stdenv.mkDerivation {
-            name = "admin-scripts";
-            version = "1.0";
-            src = ./.;
-
-            nativeBuildInputs = [ coreutils makeWrapper gnused ];
-
-            # TODO: Create dev shell
-
-            postInstall =
-              let
-                wrapperPath = lib.makeBinPath [
-                  coreutils
-                  bash
-                  git
-                  bitwarden-cli
-                  sshpass
-                  openssh
-                  findutils
-                  gnugrep
-                  gnused
-                ];
-              in
-              ''
-                for program in $out/bin/*; do
-                  wrapProgram "$program" --prefix PATH : "${wrapperPath}"
-                done
-              '';
-            installPhase = ''
-              runHook preInstall
-              mkdir -p "$out"
-              cp -r ./bin "$out"
-              cp lib.sh "$out"
-              runHook postInstall
-            '';
-          };
-        }
-        );
-
-      devShells = forAllSystems
-        (
-          { pkgs }: {
-            default = with pkgs; mkShell {
-              packages = [
-                shfmt
-                shellcheck
-                nodePackages_latest.bash-language-server
-              ];
-            };
-          }
-        );
-    };
 }
