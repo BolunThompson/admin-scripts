052210abb461a17716335fc746514ec7f0ab6b7a
Bolun Thompson <bolunthompson@ucla.edu>
2025-11-26 15:40:22 -0800

    Upgrade Python version from 3.12 to 3.14

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        python = pkgs.python312.withPackages (ppkgs: with ppkgs; [
+        python = pkgs.python314.withPackages (ppkgs: with ppkgs; [
           ansi2html
           weasyprint
         ]);
